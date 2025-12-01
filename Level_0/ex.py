4acf71e99064ee67963b6e85a5e931a41c7cc528
dong11hyun <eastmanin@gmail.com>
2025-12-01 15:51:43 +0900

    basic_1

---
+++ Level_0/ex.py
@@ -0,0 +1,19 @@
+# print("hello world")
+
+## input 안 문자열만 삽입 가능
+# name = input('what is your name????')
+
+# print("str" + "str")
+
+## import math
+#   print(math.sqrt(16))
+
+## from math import sqrt
+#   print(sqrt(16))
+
+## import pandas as pd
+#  data = pd.read_csv('file.csv')
+
+## import를 내가 만든 파일에서 할수있음
+## ex.) donghyun.py 파일에서 변수가져오기
+## import donghyun ~~
